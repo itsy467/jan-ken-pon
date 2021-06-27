a656514c644671968744a2de01692bb94d3b29c4
itsy467 <76798911+itsy467@users.noreply.github.com>
2021-06-27 16:24:47 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,63 @@
+let PTGplayer = 0
+let PTGmicrobit = 0
+input.onPinPressed(TouchPin.P0, function () {
+    PTGplayer = 1
+})
+input.onButtonPressed(Button.A, function () {
+    PTGmicrobit = randint(1, 3)
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . # # # .
+        . . . # .
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . # # # .
+        . # . . .
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(750)
+    if (PTGmicrobit == 1) {
+        basic.showString("P")
+    } else {
+        if (PTGmicrobit == 2) {
+            basic.showString("T")
+        } else {
+            basic.showString("G")
+        }
+    }
+})
+input.onPinPressed(TouchPin.P2, function () {
+    PTGplayer = 3
+})
+input.onButtonPressed(Button.AB, function () {
+    if (PTGplayer + PTGmicrobit == 3 && PTGplayer > PTGmicrobit || PTGplayer + PTGmicrobit == 5 && PTGplayer > PTGmicrobit || PTGplayer + PTGmicrobit == 4 && PTGplayer < PTGmicrobit) {
+        basic.showString("You Win!")
+    }
+    if (PTGmicrobit == PTGplayer) {
+        basic.showString("AIKO")
+        if (PTGplayer + PTGmicrobit == 3 && PTGplayer < PTGmicrobit || PTGplayer + PTGmicrobit == 5 && PTGplayer < PTGmicrobit || PTGplayer + PTGmicrobit == 4 && PTGplayer > PTGmicrobit) {
+            basic.showString("You Lose...")
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    PTGmicrobit = 0
+    PTGplayer = 0
+    basic.showString("RESET!")
+})
+input.onPinPressed(TouchPin.P1, function () {
+    PTGplayer = 2
+})
